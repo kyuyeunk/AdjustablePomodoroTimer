c5e067815e5f357e5f8be04dce3d14108d4aea11
Kyu Yeun Kim <kyuyeunk01@gmail.com>
2020-04-11 12:07:16 +0900

    Initial commit for apple watch app

---
+++ PromodoTimer for Watch Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PromodoTimer for Watch Extension
+//
+//  Created by Kyu Yeun Kim on 2020/04/11.
+//  Copyright © 2020 Kyu Yeun Kim. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+            Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PromodoTimer for Watch Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  PromodoTimer for Watch Extension
+//
+//  Created by Kyu Yeun Kim on 2020/04/11.
+//  Copyright © 2020 Kyu Yeun Kim. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
